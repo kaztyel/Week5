fefe640140eab2693472e5ddfe4098c7322025c5
kaztyel <91223017+kaztyel@users.noreply.github.com>
2021-10-29 02:20:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ week4model = pickle.load(open('week4model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('indexW4.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -29,7 +29,7 @@ def predict():
 
     output = prediction[0]
 
-    return render_template('indexw4.html', prediction_text='The flower is {}'.format(output))
+    return render_template('indexW4.html', prediction_text='The flower is {}'.format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
